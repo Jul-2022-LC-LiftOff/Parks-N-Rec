a1f7775ae36fb017010da5a370706b5255d4c0aa
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-08-11 15:04:31 -0500

    display National but can't not search by name

---
+++ src/main/ui/src/components/ParkList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Park from "./Park";
+import Loading from "./Loading";
+import { useGlobalContext } from "./context";
+
+export default function ParkList() {
+  const { parks, loading } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (parks.length < 1) {
+    return (
+      <h2 className="section-title">no Park matched your search criteria</h2>
+    );
+  }
+  return (
+    <section className="section">
+      <h2 className="section-title">parks</h2>
+      <div className="cocktails-center">
+        {parks.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+}
---
+++ src/main/ui/src/context.js
@@ -0,0 +1,58 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
+
+const url = `https://developer.nps.gov/api/v1/parks?StateCode="all"&limit=468&api_key=ABWZc9qTpwdEzXasClnO4MLAyxYugwYZeNj0GFxi`;
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [parks, setParks] = useState([]);
+
+  const fetchParks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}`);
+
+      const datas = await response.json();
+      console.log(datas);
+      const { data } = datas;
+      if (data) {
+        const newParks = data.map((item) => {
+          const { id, url, fullName, parkCode, description, images } = item;
+
+          return {
+            idPark: id,
+            name: fullName,
+            code: parkCode,
+            detaisl: description,
+            image: images,
+          };
+        });
+        setParks(newParks);
+      } else {
+        setParks([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
+  useEffect(() => {
+    fetchParks();
+  }, [searchTerm, fetchParks]);
+  return (
+    <AppContext.Provider
+      value={{ loading, parks, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
