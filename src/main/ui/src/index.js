134325259f2021ffa0b8d8667e10d54a5f788ff7
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-08-19 16:27:43 -0500

    node_modules

--- src/main/ui/src/index.js
+++ src/main/ui/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-    <App />
+     <App />
     </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
