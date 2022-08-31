ba85a2b38a3e3efc8367bf81f5a80fbfd577ef4d
analeamaesterling <analeamaesterling@gmail.com>
2022-08-30 19:49:22 -0500

    Trying to add notes.java

--- src/main/java/Models/Notes.java
+++ src/main/java/Models/Notes.java
@@ -1,2 +1,56 @@
-package Models;public class Notes {
+package Models;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String note;
+
+    public Notes() {};
+
+    public Notes (String note) {
+        this.note = note;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notes notes = (Notes) o;
+        return id == notes.id && note.equals(notes.note);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, note);
+    }
+
+    @Override
+    public String toString() {
+        return "Notes{" +
+                "id=" + id +
+                ", note='" + note + '\'' +
+                '}';
+    }
+
+
 }
