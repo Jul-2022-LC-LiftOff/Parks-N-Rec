5de7274a4d9af48663aa9efc4a92e2c967b9ce5e
analeamaesterling <analeamaesterling@gmail.com>
2022-09-06 21:01:57 -0500

    Changed handleSaveClick localhost from 8080 to 3000

--- src/main/ui/src/components/Notes/AddNote.jsx
+++ src/main/ui/src/components/Notes/AddNote.jsx
@@ -22,7 +22,7 @@ export default function AddNote({handleAddNote}) {
          event.preventDefault();
          const noteInfo={noteText}
          console.log(noteInfo);
-           fetch("http://localhost:8080/notes/addNotes",{
+           fetch("http://localhost:3000/notes/addNotes",{
               method: "POST",
               headers:{"Content-Type":"application/json"},
               body:JSON.stringify(noteText)
