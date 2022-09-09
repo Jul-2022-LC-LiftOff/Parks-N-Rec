c41f4c53f183984022d477c3d5f07f97b38e9660
analeamaesterling <analeamaesterling@gmail.com>
2022-09-08 19:48:59 -0500

    Changing port to try to fix 404 error

--- src/main/ui/src/components/Notes/AddNote.jsx
+++ src/main/ui/src/components/Notes/AddNote.jsx
@@ -22,7 +22,8 @@ export default function AddNote({handleAddNote}) {
          event.preventDefault();
          const noteInfo={noteText}
          console.log(noteInfo);
-           fetch("http://localhost:3000/notes/addNotes",{
+//            fetch("http://localhost:3000/notes/addNotes", {
+           fetch("http://localhost:3000/notes/addNotes", {
               method: "POST",
               headers:{"Content-Type":"application/json"},
               body:JSON.stringify(noteText)
