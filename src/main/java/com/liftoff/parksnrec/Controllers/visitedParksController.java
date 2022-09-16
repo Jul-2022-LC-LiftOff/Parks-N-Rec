fb52a16183bc48f51c4a2148b5340bd11b437cb1
analeamaesterling <analeamaesterling@gmail.com>
2022-09-16 11:14:14 -0500

    Fixing controller

--- src/main/java/com/liftoff/parksnrec/Controllers/visitedParksController.java
+++ src/main/java/com/liftoff/parksnrec/Controllers/visitedParksController.java
@@ -1,2 +1,28 @@
-package com.liftoff.parksnrec.Controllers;public class visitedParksController {
+package com.liftoff.parksnrec.Controllers;
+
+import com.liftoff.parksnrec.Models.VisitedParks;
+import com.liftoff.parksnrec.Service.VisitedParksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class visitedParksController {
+    @Autowired
+    private VisitedParksService visitedParksService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody VisitedParks visitedparks){
+        visitedParksService.saveParks(visitedParks);
+        return "New user is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<VisitedParks> getAllUsers(){
+        return visitedParksService.getAllParks();
+    }
 }
