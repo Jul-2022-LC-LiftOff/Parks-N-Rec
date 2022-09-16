ee7d73989dab8f73a26ee4a8b329a37af0aafb9c
analeamaesterling <analeamaesterling@gmail.com>
2022-09-16 10:33:40 -0500

    .

--- src/main/ui/src/components/UserLanding/UserLanding.jsx
+++ src/main/ui/src/components/UserLanding/UserLanding.jsx
@@ -6,8 +6,13 @@ export default function UserLanding() {
     const [userLanding, setUserLanding] = React.useState([]);
 
     React.useEffect(() => {
-
-    }
+        fetch('http://localhost:3000')
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json.data);
+            })
+            .catch((error) => console.log(error))
+    }, []);
     return <div className = "landingPage">
         <h1>Testing</h1>
     </div>;
