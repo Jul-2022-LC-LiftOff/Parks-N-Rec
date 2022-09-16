ab08fc190ad80c609a81498cf92d093a935d5654
analeamaesterling <analeamaesterling@gmail.com>
2022-09-16 10:14:29 -0500

    User landing

---
+++ src/main/ui/src/components/UserLanding/UserLanding.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react",
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function UserLanding() {
+    const [userLanding, setUserLanding] = React.useState([]);
+
+    React.useEffect(() => {
+
+    }
+
+}
