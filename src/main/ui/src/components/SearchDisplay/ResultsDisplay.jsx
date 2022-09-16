2bad582eacf5cb8ff1d2f58a93228e28caaf3c54
drew <drewbeaven405@gmail.com>
2022-09-16 12:01:25 -0500

    wonky

--- src/main/ui/src/components/SearchDisplay/ResultsDisplay.jsx
+++ src/main/ui/src/components/SearchDisplay/ResultsDisplay.jsx
@@ -6,17 +6,34 @@ import searchTerm from "../SearchByName/SearchByName";
 
 export default function ResultsDisplay() {
 	const [post, setPost] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('')
 	const [isCompleted, setIsCompleted] = useState(false);
 	const [index, setIndex] = useState(5);
 	const initialPosts = slice(post, 0, index);
-	const getData = () => {
-		fetch(
-			`https://developer.nps.gov/api/v1/parks?StateCode="all"&limit=469&api_key=${API_KEY}`
-		)
+	React.useEffect(() => {
+	  let url = `https://developer.nps.gov/api/v1/parks?StateCode="all"&limit=469&api_key=${API_KEY}`
+		fetch(url)
 			.then((res) => res.json())
 			.then((json) => setPost(json.data))
 			.catch((e) => console.log(e));
-	};
+	}, [searchTerm]);
+
+	function handleSearch(e) {
+        e.preventDefault();
+        setSearchTerm(e.target.value)
+        } if (post.length > 0) {
+            post.filter((i)  => {
+            return i.fullName.match(post);
+            });
+    }
+
+    function filterParkNames(park) {
+       if (searchTerm === ""){
+           return park
+       } if (park.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
+           return park
+       }
+    }
 
 	const loadMore = () => {
 		setIndex(index + 5);
@@ -28,14 +45,20 @@ export default function ResultsDisplay() {
 		}
 	};
 
-	useEffect(() => {
-		getData();
-	}, []);
 
 	return (
 		<div>
-			<h2 className="mb-3">Search results for {searchTerm} </h2>
-			{initialPosts.map((parkData, index) => {
+
+			<input
+              type="text"
+              placeholder="Search parks by name"
+              onChange = {handleSearch}
+            />
+
+            {post.filter((park) => {
+                return filterParkNames(park)
+            })
+			 .map((parkData, index) => {
 				return <ResultCard parkData={parkData} key={index} />;
 			})}
 			<div className="d-grid mt-3 mb-5">
--- src/main/ui/src/components/SearchDisplay/SearchResultsPage.jsx
+++ src/main/ui/src/components/SearchDisplay/SearchResultsPage.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ResultsDisplay from "./ResultsDisplay";
-import SearchPageHeroImage from "./SearchPageHeroImage";
+
 
 export default function SearchResultsPage() {
 	return (
 		<div>
-			<SearchPageHeroImage />
 			<ResultsDisplay />
 		</div>
 	);
