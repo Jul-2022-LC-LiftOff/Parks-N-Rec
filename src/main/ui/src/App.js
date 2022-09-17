f73387affd2dfec2b0083c07c200d8bb448b7e98
Jay <jaylingelbach@users.noreply.github.com>
2022-09-17 15:17:15 -0500

    Revert "More formatting"

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,16 +1,30 @@
+import SearchByName from "./components/SearchByName/SearchByName";
+import ParkNotes from "./components/Notes/ParkNotes";
+import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import Homepage from "./components/Homepage/Homepage";
 
-
 const App = () => {
+	const [notes, setNotes] = useState([]);
 
+	const addNote = (text) => {
+		const date = new Date();
+		const newNote = {
+			text: text,
+			date: date.toLocaleDateString(),
+		};
+		//spread operator (...) expands an array into its elements. Will create new array instead of updating old array
+		const newNotes = [...notes, newNote];
+		setNotes(newNotes);
+	};
 
 	return (
 		<div className="container">
 			<Header />
 			<Homepage />
 			<ParkNotes notes={notes} handleAddNote={addNote} />
+			<SearchByName />
 		</div>
 	);
 };
