690bdb05f1df948f4b17976fa0f0e1467f8dcc03
Andrew Beaven <100181471+Dbeaven405@users.noreply.github.com>
2022-09-19 18:50:03 -0500

    app.js fix

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,14 +1,26 @@
-import SearchByName from './components/SearchByName/SearchByName';
 import './App.css';
 import { Header } from './components/Header/Header';
 import Homepage from './components/Homepage/Homepage';
+import Login from './components/Header/Login.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Results from './components/Header/Results.jsx';
+import SearchByName from './components/SearchByName/SearchByName';
+import SearchByStatePage from './components/SearchByStatePage/SearchByStatePage';
 
 const App = () => {
   return (
     <div className="container">
-      <Header />
-      <Homepage />
-      <SearchByName />
+      <div>
+         <Header />
+        <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/home" element={<Homepage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/results" element={<Results />} />
+                <Route path="/searchByName" element={<SearchByName />} />
+                <Route path="/searchByState" element={<SearchByStatePage />} />
+        </Routes>
+      </div>
     </div>
   );
 };
