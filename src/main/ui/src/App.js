c6dd1d54e70cdf6284694b577612eb6a95883509
Jay Linglbach <jay.lingelbach@gmail.com>
2022-09-19 09:28:17 -0500

    minor fixes

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,18 +1,11 @@
-import "./App.css";
-import { Header } from "./components/Header/Header";
-import Homepage from "./components/Homepage/Homepage";
-
-
-const App = () => {
-
-
-	return (
-		<div className="container">
-			<Header />
-			<Homepage />
-			<ParkNotes notes={notes} handleAddNote={addNote} />
-		</div>
-	);
-};
+import './App.css';
+import { Header } from './components/Header/Header';
+import Homepage from './components/Homepage/Homepage';
+const App = () => (
+  <div className="container">
+    <Header />
+    <Homepage />
+  </div>
+);
 
 export default App;
