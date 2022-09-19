90738448c6fe89ee17bfd678d658200c5473b42e
libbypeterson <elibbypeterson@gmail.com>
2022-09-19 18:21:08 -0500

    changed pic out with generic image, down to 2 cards

--- src/main/ui/src/components/ParkView/ParkProfile.jsx
+++ src/main/ui/src/components/ParkView/ParkProfile.jsx
@@ -6,111 +6,111 @@ import ThingsToDoAccordion from "./ThingsToDoAccordion";
 import VisitedParkSwitch from "./VisitedParkSwitch";
 
 export default function ParkInfo() {
-	const [parkInfo, setParkInfo] = useState([]);
+  const [parkInfo, setParkInfo] = useState([]);
 
-	let parkCode = "yell";
-	useEffect(() => {
-		//parkCode should change depending on user choice
-		let url =
-			"https://developer.nps.gov/api/v1/parks?parkCode=" +
-			parkCode +
-			"&api_key=IJ1FjAb4SkwSyXOqFhyqxTM3xg66eDcMQoDjKS16";
-		fetch(url)
-			.then((response) => response.json())
-			.then((json) => {
-				setParkInfo(json.data[0]);
-			})
-			.catch((error) => console.log(error));
-	}, []);
+  let parkCode = "yell";
+  useEffect(() => {
+    //parkCode should change depending on user choice
+    let url =
+      "https://developer.nps.gov/api/v1/parks?parkCode=" +
+      parkCode +
+      "&api_key=IJ1FjAb4SkwSyXOqFhyqxTM3xg66eDcMQoDjKS16";
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setParkInfo(json.data[0]);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-	return (
-		<div className="overall-div">
-			<ProfileHeroImage parkInfo={parkInfo} />
-			<Container className="park-description">
-				<div> {parkInfo.description} </div>
-			</Container>
-			<VisitedParkSwitch />
+  return (
+    <div className="overall-div">
+      <ProfileHeroImage parkInfo={parkInfo} />
+      <Container className="park-description">
+        <div> {parkInfo.description} </div>
+      </Container>
+      <VisitedParkSwitch />
 
-			<ThingsToDoAccordion parkCode={parkCode} parkInfo={parkInfo} />
+      <ThingsToDoAccordion parkCode={parkCode} parkInfo={parkInfo} />
 
-			<Container className="cards-container">
-				<div className="row row-cols-1 row-cols-md-4 g-4">
-					<div className="col">
-						<div className="card h-50">
-							<div className="card text-white mb-3 bg-dark">
-								<img
-									className="img-fluid"
-									src="https://images.unsplash.com/photo-1604357209793-fca5dca89f97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
-									alt="gps on phone"
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Location</h5>
-									{/* <a href={parkDirectionsUrl} className="btn btn-success">
-										Directions
-									</a> */}
-								</div>
-							</div>
-						</div>
-					</div>
+      <Container className="cards-container">
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+            <div className="card h-50">
+              <div className="card text-white mb-3 bg-dark">
+                <img
+                  className="img-fluid"
+                  src="https://images.unsplash.com/photo-1604357209793-fca5dca89f97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+                  alt="gps on phone"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Location</h5>
+                  <a href={parkInfo.directionsUrl} className="btn btn-success">
+                    Directions
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
 
-					<div className="col">
-						<div className="card h-50">
-							<div className="card text-white bg-dark mb-3">
-								<img
-									className="img-fluid"
-									src="https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjV8fHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-									alt="Card image cap"
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Visit the Website</h5>
-									{/* <a href={parkWebsite} className="btn btn-success">
-										Official Website
-									</a> */}
-								</div>
-							</div>
-						</div>
-					</div>
+          {/* <div className="col">
+            <div className="card h-50">
+              <div className="card text-white bg-dark mb-3">
+                <img
+                  className="img-fluid"
+                  src="https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjV8fHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Visit the Website</h5>
+                  <a href={parkInfo.url} className="btn btn-success">
+                    Official Website
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div> */}
 
-					<div className="col">
-						<div className="card h-50">
-							<div className="card text-white bg-dark mb-3">
-								<img
-									className="img-fluid"
-									src="https://images.unsplash.com/photo-1602562775100-bdff2108c7d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YWxlcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-									alt="safety cone"
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Park Alerts</h5>
-									<h6 className="card-text">
-										Choose something, alerts not included in API
-									</h6>
-								</div>
-							</div>
-						</div>
-					</div>
+          {/* <div className="col">
+            <div className="card h-50">
+              <div className="card text-white bg-dark mb-3">
+                <img
+                  className="img-fluid"
+                  src="https://images.unsplash.com/photo-1602562775100-bdff2108c7d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YWxlcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+                  alt="safety cone"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Park Alerts</h5>
+                  <h6 className="card-text">
+                    Choose something, alerts not included in API
+                  </h6>
+                </div>
+              </div>
+            </div>
+          </div> */}
 
-					<div className="col">
-						<div className="card h-50">
-							<div className="card text-white bg-dark mb-3">
-								<img
-									className="img-fluid"
-									src="https://images.unsplash.com/photo-1534794048419-48e110dca88e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYXRoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-									alt="umbrellas"
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Weather</h5>
-									{/* <h6 className="card-text">{parkWeather}</h6> */}
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<Container className="learn-more-header">
-					<a className="font-weight-bold" href={parkInfo?.url}>
-						Learn more about {parkInfo?.fullName} on the NPS website.
-					</a>
-				</Container>
-			</Container>
-		</div>
-	);
+          <div className="col">
+            <div className="card h-50">
+              <div className="card text-white bg-dark mb-3">
+                <img
+                  className="img-fluid"
+                  src="https://images.unsplash.com/photo-1534794048419-48e110dca88e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYXRoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+                  alt="umbrellas"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Weather</h5>
+                  <h6 className="card-text">{parkInfo.weatherInfo}</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Container className="learn-more-header">
+          <a className="font-weight-bold" href={parkInfo?.url}>
+            Learn more about {parkInfo?.fullName} on the NPS website.
+          </a>
+        </Container>
+      </Container>
+    </div>
+  );
 }
