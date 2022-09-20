83068f77589d7256188018da304d8f9adb5a81dc
analeamaesterling <analeamaesterling@gmail.com>
2022-09-19 20:14:32 -0500

    .

--- src/main/ui/src/components/UserLanding/UserLanding.jsx
+++ src/main/ui/src/components/UserLanding/UserLanding.jsx
@@ -5,40 +5,58 @@ import { useEffect } from "react";
 export default function UserLanding() {
     const [checked, setChecked] = React.useState([]);
     const [userLanding, setUserLanding] = React.useState([]);
+    const [visited, setVisited] = React.useState("");
 
 
-    const handleCheckBox = () => {
-              setChecked(!checked);
-         };
 
-    const handleChange=(e)=> {
-        handleCheckBox()
-        e.preventDefault()
-        const userInfo = {userLanding}
+
+    const handleCheckBox = (e) => {
+    e.preventDefault()
+        const userInfo = {visited}
         console.log(userInfo)
             fetch("http://localhost:3000/user/parkAdded", {
-                method: "GET",
+                method: "POST",
                 headers:{"Content-Type": "application/json"},
+                body: JSON.stringify(userInfo)
             }).then(() => {
                 setUserLanding(userInfo)
-                console.log(userLanding + "Hello")
                 console.log(userInfo)
             })
             .catch((error) => console.log(error))
-    }
+
+         };
+
+//     const handleChange=(e)=> {
+//         e.preventDefault()
+//         const userInfo = {visited}
+//         console.log(userInfo)
+//             fetch("http://localhost:8080/user/parkAdded", {
+//                 method: "POST",
+//                 headers:{"Content-Type": "application/json"},
+//                 body: JSON.stringify(userInfo)
+//             }).then(() => {
+//                 setUserLanding(userInfo)
+//                 console.log(userInfo)
+//             })
+//             .catch((error) => console.log(error))
+//     }
 
 
 
     return (
-    <div className = "landingPage">
+    <div className="landingPage">
         <h1>
            TestingTesting
         </h1>
 
+    <form method="post">
         <label>
-            <input type = "checkbox" checked={checked} onChange = {handleChange} />
+{/*             <input type="checkbox" onClick={(e)=>setVisited(e.target.value)} /> */}
+<input type="checkbox" onClick={handleCheckBox} />
         </label>
+    </form>
     </div>
 );
 }
+
 
