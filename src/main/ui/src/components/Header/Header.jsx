c96ed008efc1d5d0e696f2719964efbcf56cda63
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-09-19 19:08:11 -0500

    test6

--- src/main/ui/src/components/Header/Header.jsx
+++ src/main/ui/src/components/Header/Header.jsx
@@ -1,25 +1,26 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import SearchBar from '../SearchBar/SearchBar';
 import StateDropdown from './StateDropdown';
-import Login from '../Login/Login';
-import Register from '../Register/Register';
 
 export const Header = () => {
   return (
     <Navbar variant="light" bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#home">Park It</Navbar.Brand>
+        <Navbar.Brand href="/Home">Park It</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar" />
         <Navbar.Collapse id="navbar">
           <Nav>
             <SearchBar />
             <StateDropdown />
           </Nav>
-          <Login />
-          <Register />
         </Navbar.Collapse>
+        <Link to="/login" className="login-page">
+          {' '}
+          Login | Register{' '}
+        </Link>
       </Container>
     </Navbar>
   );
