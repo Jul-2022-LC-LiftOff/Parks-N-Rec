ad82bb2ed604c04e7471f43d0391ed73795d169b
emkharr <ekharrison.art@gmail.com>
2022-09-20 20:22:20 -0600

    started styling

--- src/main/ui/src/components/Header/Header.jsx
+++ src/main/ui/src/components/Header/Header.jsx
@@ -1,27 +1,34 @@
-import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import SearchBar from '../SearchBar/SearchBar';
-import StateDropdown from './StateDropdown';
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import SearchBar from "../SearchBar/SearchBar";
+import StateDropdown from "./StateDropdown";
 
 export const Header = () => {
-  return (
-    <Navbar variant="light" bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="/Home">Park It</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar" />
-        <Navbar.Collapse id="navbar">
-          <Nav>
-            <SearchBar />
-            <StateDropdown />
-          </Nav>
-        </Navbar.Collapse>
-        <Link to="/login" className="login-page">
-          {' '}
-          Login | Register{' '}
-        </Link>
-      </Container>
-    </Navbar>
-  );
+	return (
+		<Navbar variant="light" bg="light" expand="lg">
+			<Container fluid>
+				<Navbar.Brand href="/Home">
+					<img
+						src="parkLogo.png"
+						width="30"
+						height="30"
+						className="d-inline-block align-top"
+						alt="React Bootstrap logo"
+					></img>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="navbar" />
+				<Navbar.Collapse id="navbar">
+					<Nav>
+						<StateDropdown />
+					</Nav>
+				</Navbar.Collapse>
+				<Link to="/login" className="login-page">
+					{" "}
+					Login | Register{" "}
+				</Link>
+			</Container>
+		</Navbar>
+	);
 };
