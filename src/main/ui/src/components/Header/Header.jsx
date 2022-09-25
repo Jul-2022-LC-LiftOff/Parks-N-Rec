c4f4ded987437383eb0c00705ccfbc4a2b4a259b
analeamaesterling <analeamaesterling@gmail.com>
2022-09-25 12:50:35 -0500

    Merges fixed

--- src/main/ui/src/components/Header/Header.jsx
+++ src/main/ui/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+// import { Container, Nav, Navbar } from "react-bootstrap";
 import LoginPage from "./Login";
 import SearchBar from "../SearchBar/SearchBar";
 import StateDropdown from "./StateDropdown";
